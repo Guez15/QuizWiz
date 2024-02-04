ef9f26e487e69a430f9ba29515dd4f04d7b0bcdd
Guenda <132022308+GueMorr@users.noreply.github.com>
2024-02-04 13:49:29 +0100

    Prova con React Native

---
+++ QuizWiz/app/src/main/java/com/pascal/quizwiz/logIn.java
@@ -0,0 +1,11 @@
+package com.pascal.quizwiz;
+
+import android.os.Bundle;
+
+public class logIn {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.logIn);
+    }
+}
