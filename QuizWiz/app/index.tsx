6892340f4f23f9d2e779361ed54575e1123dc13f
RudyMartucci <rudy.martucciortega@studenti.iispascal.it>
2024-02-17 18:53:37 +0100

    Creazione di Login e spostamento tra Home e Login

--- QuizWiz/app/index.tsx
+++ QuizWiz/app/index.tsx
@@ -1,7 +1,14 @@
-import {View,Text} from "react-native";
+import {View,Text, Pressable} from "react-native";
+import React from 'react';
+import { router } from "expo-router";
 
 const HomePage = ()=>{
-    return <View><Text>Home page</Text></View>;
+    return (<View>
+        <Text>Home page</Text>
+        <Pressable onPress={() => router.push("login")}>
+            <Text>Pagina Login</Text>
+        </Pressable>
+    </View>);
 }
 
 export default HomePage;
---
+++ QuizWiz/app/login.tsx
@@ -0,0 +1,11 @@
+import {View,Text} from 'react-native';
+
+const Login = () =>{
+    return <View>
+        <Text>
+            Bo
+        </Text>
+    </View>;
+}
+
+export default Login;
