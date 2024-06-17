e8f640c9d0e98cfdb1a405bd562ee1293a9f14ae
QuizzWizz <167426573+QuizzWizz@users.noreply.github.com>
2024-06-17 22:55:20 +0200

    Update index.php

--- QuizWiz/Index.php
+++ QuizWiz/Index.php
@@ -1,19 +1,28 @@
 <html>
     <head>
         <title>QuizWiz - Home</title>
-        <link rel="stylesheet" href="css/home.css">
         <link rel="stylesheet" href="css/colors.css">
+        <link rel="stylesheet" href="css/homeindex.css">
+        <link rel="icon" href="img/logo.png" type="image/x-icon">
+    	<link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     </head>
     <body>
         <div id="wrapper">
             <div id='content'>
             	<section id="pres">
-
+					<div id="titolo">
+                    	<h1>BENVENUTO SU</h1>
+                    	<img src="img/logo.png" alt="logo" id="log">
+                        <p>La Web App al passo con il tuo ripasso!</p>
+                    </div>
             	</section>
+                <div id='sections'>
+
+                </div>
             </div>
         	<?php include_once("htmlBlocks/footer.html");?>
         </div>
     </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="js/home.js"></script>
-</html>
+</html>
